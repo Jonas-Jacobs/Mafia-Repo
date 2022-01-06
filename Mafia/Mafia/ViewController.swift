8d15f6b1c5dd32bf40e4c786120594626143624f
Jonas Jacobs <Jonas.jacobs6475@stu.metc.edu>
2022-01-06 08:42:43 -0700

    Starting project
    There is no code here

---
+++ Mafia/Mafia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mafia
+//
+//  Created by Jonas Jacobs on 1/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
