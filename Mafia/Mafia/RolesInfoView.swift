70952fe306823edaf4fa92972af42d3ec6f10012
Reece Olsen <olsenre17@gmail.com>
2022-01-10 09:52:22 -0700

    ro-RolesPage
    began work on descriptions and displaying the information on roles

---
+++ Mafia/Mafia/RolesInfoView.swift
@@ -0,0 +1,36 @@
+//
+//  RolesInfoView.swift
+//  Mafia
+//
+//  Created by Reece Olsen on 1/10/22.
+//
+
+import SwiftUI
+let predefinedMafiaRoles = [
+    Mafia(role: "Mafia", description: "Votes with other mafia members to attempt to kill a player."),
+    Mafia(role:"Doctor", description: "Selects somebody to save"),
+    Mafia(role:"Villager", description: "Has no specific actions, participates in town votes"),
+    Mafia(role:"Detective", description: "Investigates one person to discover who they side on.")
+]
+struct RolesInfoView: View {
+    var body: some View {
+        List{
+            ForEach(predefinedMafiaRoles) { mafiaRoles in
+                VStack(alignment: .leading){
+                    
+                    
+                    Text(mafiaRoles.role)
+                        .bold()
+                    Text(mafiaRoles.description)
+                    
+                }
+            }
+        }
+    }
+}
+
+struct RolesInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        RolesInfoView()
+    }
+}
