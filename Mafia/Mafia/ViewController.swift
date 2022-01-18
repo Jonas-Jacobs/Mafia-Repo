b83800af1523a34d542e7ff12063a06fe6cff71a
Reece Olsen <olsenre17@gmail.com>
2022-01-18 09:51:16 -0700

    made it work in simulator

--- Mafia/Mafia/ViewController.swift
+++ Mafia/Mafia/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 //Loading Screen View Controller
 class ViewController: UIViewController {
@@ -13,6 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let controller = UIHostingController(rootView: MafiaMainScreen())
+        controller.view.translatesAutoresizingMaskIntoConstraints = false
+        self.addChild(controller)
+        self.view.addSubview(controller.view)
+        controller.didMove(toParent: self)
+        NSLayoutConstraint.activate([
+                        controller.view.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+                           controller.view.heightAnchor.constraint(equalTo: self.view.heightAnchor),
+                           controller.view.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+                           controller.view.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+                           ])
     }
     
     
