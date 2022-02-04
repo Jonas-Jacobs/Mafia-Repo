72a4096f1474387669013b741554a3eb33f7b826
Reece Olsen <olsenre17@gmail.com>
2022-02-04 09:59:25 -0700

    mainscreen work

--- Mafia/Mafia/Swift-UI Mafia Files/MafiaMainScreen.swift
+++ Mafia/Mafia/Swift-UI Mafia Files/MafiaMainScreen.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct MafiaMainScreen: View {
-    let bloodRed = Color(red: 105/255, green: 5/255, blue: 0/255)
-    let richBlack = Color(red: 12/255, green: 18/255, blue: 12/255)
+    let bloodRed = Color(red: 50/255, green: 2/255, blue: 2/255)
+    let richBlack = Color(red: 190/255, green: 1/255, blue: 9/255)
     var body: some View {
         NavigationView{
             ZStack{
@@ -18,7 +18,17 @@ struct MafiaMainScreen: View {
                                endPoint: .top)
                 VStack{
                     ZStack{
-                        Color(red: 180/255, green: 27/255, blue: 30/255)
+                        Color(red: 0/255, green: 0/255, blue: 0/255)
+                            .frame(width: 300, height: 100, alignment: .center)
+                            .cornerRadius(8)
+                        Text("Mafia")
+                            .font(.system(size: 100, weight: .bold))
+                            .foregroundColor(Color(red: 0.6, green: 0, blue: 0))
+                            
+                    }
+                    
+                    ZStack{
+                        Color(red: 0/255, green: 0/255, blue: 0/255)
                             .frame(width: 200, height: 50, alignment: .center)
                             .cornerRadius(4)
                         NavigationLink {
@@ -26,10 +36,10 @@ struct MafiaMainScreen: View {
                         } label: {
                             Text("Create new game")
                         }
-                        .foregroundColor(Color.black)
+                        .foregroundColor(Color(red: 255/255, green: 0, blue: 0))
                     }
                     ZStack{
-                        Color(red: 180/255, green: 27/255, blue: 30/255)
+                        Color(red: 0/255, green: 0/255, blue: 0/255)
                             .frame(width: 200, height: 50, alignment: .center)
                             .cornerRadius(4)
                         NavigationLink {
@@ -37,7 +47,7 @@ struct MafiaMainScreen: View {
                         } label: {
                             Text("Learn rolls")
                         }
-                        .foregroundColor(Color.black)
+                        .foregroundColor(Color(red: 1, green: 0, blue: 0))
                     }
                 }
             } .ignoresSafeArea()
